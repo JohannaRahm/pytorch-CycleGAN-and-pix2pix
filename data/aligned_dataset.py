b033e132cf900871c2e0159deb44c42bd2d437d0
JohannaRahm <jojorahm@gmx.de>
2022-12-08 19:11:49 +0100

    1

--- data/aligned_dataset.py
+++ data/aligned_dataset.py
@@ -51,15 +51,15 @@ class AlignedDataset(BaseDataset):
         B = AB.crop((w2, 0, w, h))
 
         # apply the same transform to both A and B
-        # transform_params = get_params(self.opt, A.size)
-        # A_transform = get_transform(self.opt, transform_params, grayscale=(self.input_nc == 1))
-        # B_transform = get_transform(self.opt, transform_params, grayscale=(self.output_nc == 1))
-        #
-        # A = A_transform(A)
-        # B = B_transform(B)
-        # print("A_transform", A)
-        # img_matrix = np.array(A)
-        # print(img_matrix, np.unique(img_matrix))
+        transform_params = get_params(self.opt, A.size)
+        A_transform = get_transform(self.opt, transform_params, grayscale=(self.input_nc == 1))
+        B_transform = get_transform(self.opt, transform_params, grayscale=(self.output_nc == 1))
+
+        A = A_transform(A)
+        B = B_transform(B)
+        print("A_transform", A)
+        img_matrix = np.array(A)
+        print(img_matrix, np.unique(img_matrix))
 
         return {'A': A, 'B': B, 'A_paths': AB_path, 'B_paths': AB_path}
 
